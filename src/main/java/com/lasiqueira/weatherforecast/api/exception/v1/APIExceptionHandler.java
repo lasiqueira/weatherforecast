80b0ea004c6cd91e0c3f3c61ad548919c15d2fb3
Lucas Siqueira <lucas.asiq@gmail.com>
2018-10-21 22:44:12 -0300

    fixed error handling

--- src/main/java/com/lasiqueira/weatherforecast/api/exception/v1/APIExceptionHandler.java
+++ src/main/java/com/lasiqueira/weatherforecast/api/exception/v1/APIExceptionHandler.java
@@ -11,10 +11,11 @@ import org.springframework.web.context.request.WebRequest;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
 @ControllerAdvice
 public class APIExceptionHandler {
-
+/*
     @ExceptionHandler({CityNotFoundException.class, JsonParseException.class, DateTimeParseException.class, InvalidFormatException.class, IOException.class, URISyntaxException.class, Exception.class})
     public final ResponseEntity<ApiError> handleException(Exception ex, WebRequest request) {
 
@@ -29,6 +30,28 @@ public class APIExceptionHandler {
             HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
             return new ResponseEntity<>(new ApiError(ex.getMessage()), status);
         }
+    }
+    */
+
+    @ExceptionHandler(CityNotFoundException.class) public ResponseEntity < ApiError > notFoundException(final CityNotFoundException e) {
+        return error(e, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(JsonParseException.class) public ResponseEntity < ApiError > jsonParseException(final JsonParseException e) {
+        return error(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IOException.class) public ResponseEntity < ApiError > ioException(final IOException e) {
+        return error(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(URISyntaxException.class) public ResponseEntity < ApiError > uriException(final URISyntaxException e) {
+        return error(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity <ApiError> error(final Exception exception, final HttpStatus httpStatus) {
+        final String message = Optional.of(exception.getMessage()).orElse(exception.getClass().getSimpleName());
+        return new ResponseEntity < > (new ApiError(message), httpStatus);
     }
 
 }
--- src/main/java/com/lasiqueira/weatherforecast/api/exception/v1/CityNotFoundException.java
+++ src/main/java/com/lasiqueira/weatherforecast/api/exception/v1/CityNotFoundException.java
@@ -1,6 +1,6 @@
 package com.lasiqueira.weatherforecast.api.exception.v1;
 
-public class CityNotFoundException extends Throwable {
+public class CityNotFoundException extends Exception {
     public CityNotFoundException(String message) {
         super(message);
     }
