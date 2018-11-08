6d8fb8cf804d77276dfc5a37bb99cd7ff27bc133
Lucas Siqueira <lucas.asiq@gmail.com>
2018-11-08 21:50:56 -0200

    cleaning up comments

--- src/main/java/com/lasiqueira/weatherforecast/api/exception/v1/APIExceptionHandler.java
+++ src/main/java/com/lasiqueira/weatherforecast/api/exception/v1/APIExceptionHandler.java
@@ -1,37 +1,17 @@
 package com.lasiqueira.weatherforecast.api.exception.v1;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
 @ControllerAdvice
 public class APIExceptionHandler {
-/*
-    @ExceptionHandler({CityNotFoundException.class, JsonParseException.class, DateTimeParseException.class, InvalidFormatException.class, IOException.class, URISyntaxException.class, Exception.class})
-    public final ResponseEntity<ApiError> handleException(Exception ex, WebRequest request) {
-
-        if (ex.getCause() instanceof CityNotFoundException) {
-            HttpStatus status = HttpStatus.NOT_FOUND;
-
-            return new ResponseEntity<>(new ApiError(ex.getMessage()), status);
-        } else if (ex.getCause() instanceof JsonParseException) {
-            HttpStatus status = HttpStatus.BAD_REQUEST;
-            return new ResponseEntity<>(new ApiError(ex.getMessage()), status);
-        } else {
-            HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-            return new ResponseEntity<>(new ApiError(ex.getMessage()), status);
-        }
-    }
-    */
 
     @ExceptionHandler(CityNotFoundException.class) public ResponseEntity < ApiError > notFoundException(final CityNotFoundException e) {
         return error(e, HttpStatus.NOT_FOUND);
